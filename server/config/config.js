400ddfa381c5b89eb0f3cdd042b3f350f4cc2eab
Ramon Rue <rrue@clic-soft.net>
2019-03-20 18:47:29 +0100

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,9 +16,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 let urlDB;
 
 if (process.env.NODE_ENV == 'dev') {
-    urlDB='mongodb://localhost:27017/cafe'
+    urlDB='mongodb://localhost:27017/cafe';
 } else {
-    urlDB='mongodb+srv://clicsoft:woFnrbGzMHGeKuyT@cluster0-34die.mongodb.net/cafe'
+    urlDB=process.env.MONGO_URI;
 }
 
 process.env.URLDB=urlDB;
